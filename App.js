fd86fcfe7d2fc240c621e2abfba42b2325a4a588
khush2407 <20it098@charusat.edu.in>
2022-09-13 12:37:24 +0530

    Created Initial UI

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
-import NavigationBar from './components/NavigationBar';
-import Footer from './components/Footer';
-import Container from 'react-bootstrap/Container';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+// import NavigationBar from './components/NavigationBar';
+// import Footer from './components/Footer';
+// import Container from 'react-bootstrap/Container';
+// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 
 function App() {
