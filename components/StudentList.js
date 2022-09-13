d4a740bbe89cccb1cbd62d51c57aaf81b829f0ce
khush2407 <20it098@charusat.edu.in>
2022-09-13 13:11:05 +0530

    Added Alert Toast

--- components/StudentList.js
+++ components/StudentList.js
@@ -36,6 +36,7 @@ export default function StudentList() {
         <Card.Body>
           <Table striped bordered hover>
             <thead>
+                
               <tr>
                 <th>Student Id</th>
                 <th>Student Name</th>
@@ -55,6 +56,7 @@ export default function StudentList() {
                   <td>{student.name}</td>
                   <td>{student.address}</td>
                   <td>
+
                     <ButtonGroup>
                     <Link to={"/student/"+student.id}><Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button></Link>{ ' '}                      <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this,student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
                       {/* <Button size="sm" variant="outline-danger" onClick={()=>deleteStudent(student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button> */}                 
